61b5bb530ce5e330cc081f7af28ad8331f19ddc2
izakdvlpr <izakdvlpr@gmail.com>
2022-10-19 20:18:15 -0300

    feat: add user fields

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,14 +1,14 @@
 package models
 
-type User struct {
-	FullName string
-	Email    string
-	Password string
-	Phone    string
-	Document string
-	address  []Address
-}
+import "time"
 
-type Address struct {
-	Number int
+type User struct {
+	ID        string    `gorm:"primary_key" form:"id" json:"id"`
+	FullName  string    `form:"full_name" json:"full_name" binding:"required"`
+	Email     string    `form:"email" json:"email" binding:"required"`
+	Password  string    `form:"password" json:"password" binding:"required"`
+	Phone     string    `form:"phone" json:"phone" binding:"required"`
+	Document  string    `form:"document" json:"document" binding:"required"`
+	CreatedAt time.Time `gorm:"autoCreateTime" form:"created_at" json:"created_at"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime" form:"update_at" json:"update_at"`
 }
